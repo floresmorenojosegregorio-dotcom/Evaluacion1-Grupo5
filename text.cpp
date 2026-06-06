683a236443f612cd2b3b80083ebf1a4f64f240e7
Josegamer5643 <floresmorenojosegregorio@gmail.com>
2026-06-06 13:28:43 -0400

    Eliminar archivo sobrante llamado text.cpp

--- text.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std; 
-int main()
-    cout << "Bienvenidos, "
-            "somos el Grupo 5" << endl;
-    
-    string nombreUsuario;
-    cout << "\nIndique su nombre: ";
