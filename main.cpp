bf26ec68c7235100064caa16b739bb2d739878e9
Josegamer5643 <floresmorenojosegregorio@gmail.com>
2026-06-05 21:01:20 -0400

    [FEAT] Inicializar estructura principal de main.cpp con banner de bienvenida

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    cout << "====================================================\n";
+    cout << "          Bienvenidos, somos el Grupo 5.\n";
+    cout << "====================================================\n"; 
