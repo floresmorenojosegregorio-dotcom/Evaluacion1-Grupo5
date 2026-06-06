75b225948fffe146ef634ba17a53889fa9688a15
bryanalejandronievesutrera <bryanalejandronievesutrera@gmail.com>
2026-06-05 21:13:13 -0400

    [ADD] Inclusión de lista de miembros usando caracteres de salto de línea (\n)

--- main.cpp
+++ main.cpp
@@ -16,3 +16,7 @@ string nombreUsuario;
 cout << "\n¡Bienvenido/a, " << nombreUsuario << "!\n\n"
          << "Listado de integrantes del Grupo 5 que participaron:\n"
          << "----------------------------------------------------\n";
+cout << " 1. Edixon Castro        -  V-31210293\n"
+         << " 2. Jorge Andrade        -  V-31174882\n"
+         << " 3. Bryan Nieves         -  V-32589282\n"
+         << " 4. Felix Perez          -  V-31583083\n";
