f0ff1d92636ed501b7e1e58e252d61167e5176d3
iscesaaar07 <villaloboscesar313@gmail.com>
2026-06-05 21:05:15 -0400

    [FEAT] "se añadió solicitud de nombre de usuario"

--- main.cpp
+++ main.cpp
@@ -8,3 +8,8 @@ int main() {
     cout << "====================================================\n";
     cout << "          Bienvenidos, somos el Grupo 5.\n";
     cout << "====================================================\n"; 
+
+string nombreUsuario;
+    cout << "\nPor favor, ingrese su nombre para continuar: ";
+    getline(cin, nombreUsuario);
+
