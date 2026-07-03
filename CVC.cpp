c871c595f49009a7d6c22214faf904b29cd4e120
iscesaaar07 <villaloboscesar313@gmail.com>
2026-07-03 17:11:31 -0400

    [ADD] agregar librerías cpr y json-c para integración de la API

--- CVC.cpp
+++ CVC.cpp
@@ -5,3 +5,11 @@
 ‎#include <iomanip>
 ‎// Necesario para std::system
 ‎#include <cstdlib>
+
+‎//Incluye lo necesario para trabajar con archivos JSON
+‎#include <json-c/json.h>
+‎//Incluye las funciones necesarias para gestionar las respuestas HTTP
+‎#include <cpr/cpr.h> 
+‎#undef max 
+‎//Retiramos la definicion de max que incluye cpr junto a un archivo de cabecera, ya que ocasiona conflictos con la funcion estatica max() perteneciente a numeric_limits
+‎
